928d9ce4021ddb7781e3652b3ccd1c5c192bb0b8
himanshu-khati <himanshukhati94@gmail.com>
2023-11-14 01:22:32 +0530

    ui changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -55,67 +55,65 @@ function Header() {
     });
   }, []);
   return (
-    <header className="absolute  z-50 px-2 sm:px-4 sm:py-2 py-3  backdrop-blur-md bg-black/60  w-full top-0 ">
-      <div className="container mx-auto ">
-        <div className="w-full flex justify-between  items-center ">
-          <div className="logo-area sm:w-6/12 w-3/12">
-            <div className="flex justify-start items-center ">
-              <p className="text-cinewhiz tracking-widest  sm:font-bold sm:text-3xl font-semibold text-xl">
-                CINEWHIZ
-              </p>
-            </div>
+    <header className="container absolute  z-50 px-2 sm:px-4 sm:py-2 py-3  backdrop-blur-md bg-black/60  w-full top-0 ">
+      <div className="w-full flex justify-between  items-center ">
+        <div className="logo-area sm:w-6/12 w-3/12">
+          <div className="flex justify-start items-center ">
+            <p className="text-cinewhiz tracking-widest  sm:font-bold sm:text-3xl font-semibold text-xl">
+              CINEWHIZ
+            </p>
           </div>
+        </div>
 
-          {user && (
-            <div className="user-area sm:w-6/12 9/12">
-              <div className=" flex justify-end items-center sm:gap-3 gap-2 ">
-                {showGptSearch && (
-                  <div className="language-options">
-                    <select
-                      name=""
-                      id=""
-                      className="text-gray-300 bg-black cursor-pointer  bg-opacity-5  text-sm sm:text-base"
-                      onChange={handleLanguageChange}
-                    >
-                      {SUPPORTED_LANGUAGES.map((language) => (
-                        <option
-                          key={language.identifier}
-                          value={language.identifier}
-                          className="text-black"
-                        >
-                          {language.name}
-                        </option>
-                      ))}
-                    </select>
-                  </div>
-                )}
-                <div className="gpt-search">
-                  <button
-                    className="text-gray-300 bg-white bg-opacity-10 border hover:bg-white hover:bg-opacity-25 sm:p-2 py-0.5 px-1 rounded"
-                    onClick={handleGptSearch}
+        {user && (
+          <div className="user-area sm:w-6/12 9/12">
+            <div className=" flex justify-end items-center sm:gap-3 gap-2 ">
+              {showGptSearch && (
+                <div className="language-options">
+                  <select
+                    name=""
+                    id=""
+                    className="text-gray-300 bg-black cursor-pointer  bg-opacity-5  text-sm sm:text-base"
+                    onChange={handleLanguageChange}
                   >
-                    {showGptSearch ? "Home" : "GPT Search"}
-                  </button>
+                    {SUPPORTED_LANGUAGES.map((language) => (
+                      <option
+                        key={language.identifier}
+                        value={language.identifier}
+                        className="text-black"
+                      >
+                        {language.name}
+                      </option>
+                    ))}
+                  </select>
                 </div>
-                <div className="user-icon">
-                  <img
-                    className="user-avatar sm:w-auto sm:h-auto w-4 h-4 "
-                    src={user?.photoURL}
-                    alt=""
-                  />
-                </div>
-                <div className="sign-out">
-                  <p
-                    className="text-white sm:text-base sm:font-semibold text-sm cursor-pointer "
-                    onClick={handleSignOut}
-                  >
-                    Sign Out
-                  </p>
-                </div>
+              )}
+              <div className="gpt-search">
+                <button
+                  className="text-gray-300 bg-white bg-opacity-10 border hover:bg-white hover:bg-opacity-25 sm:p-2 py-0.5 px-1 rounded"
+                  onClick={handleGptSearch}
+                >
+                  {showGptSearch ? "Home" : "GPT Search"}
+                </button>
+              </div>
+              <div className="user-icon">
+                <img
+                  className="user-avatar sm:w-auto sm:h-auto w-4 h-4 "
+                  src={user?.photoURL}
+                  alt=""
+                />
+              </div>
+              <div className="sign-out">
+                <p
+                  className="text-white sm:text-base sm:font-semibold text-sm cursor-pointer "
+                  onClick={handleSignOut}
+                >
+                  Sign Out
+                </p>
               </div>
             </div>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     </header>
   );
